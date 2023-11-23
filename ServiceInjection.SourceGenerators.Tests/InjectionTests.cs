f13e672f31f5efa2524154a9b14fef2627376798
Matthias Voigt <m.voigt@code-ix.de>
2023-11-23 18:16:55 +0100

    First dummy test

---
+++ ServiceInjection.SourceGenerators.Tests/InjectionTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeIX.ServiceInjection.SourceGenerators;
+
+namespace ServiceInjection.SourceGenerators.Tests;
+
+[TestClass]
+public class InjectionTests
+{
+    [TestMethod]
+    [Description("Test the default values of the Injection class.")]
+    public void TestMyMethod()
+    {
+        // Arrange
+        var injection = new Injection();
+
+        // Act
+
+        // Assert
+        Assert.IsNotNull(injection);
+        Assert.IsFalse(injection.Required);
+        Assert.IsNull(injection.Type);
+        Assert.IsNull(injection.InjectedType);
+    }
+}
